e2905a38674957944cbb41e4bb48bf4e2f972cf8
Elvis567890 <mbaziiraelvis0@gmail.com>
2026-08-11 20:40:43 +0300

    Update pre_deploy.py

--- pre_deploy.py
+++ pre_deploy.py
@@ -1,5 +1,4 @@
-from scrapper import app, db, create_admin_user
-
+from scrapper import app, db, create_admin_user  # double 'p'
 with app.app_context():
     db.create_all()
     create_admin_user()
