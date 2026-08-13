c568a8ad96aa9d5ff3e2f17e0d7b6a5a320df794
Elvis567890 <mbaziiraelvis0@gmail.com>
2026-08-13 10:40:39 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,29 @@
+import eventlet
+eventlet.monkey_patch()
+
+from gunicorn.app.base import BaseApplication
+from scraper import app # Your Flask app is named 'app'
+
+class StandaloneApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+if __name__ == '__main__':
+    options = {
+        'bind': '0.0.0.0:8080',
+        'workers': 1,
+        'worker_class': 'eventlet',
+        'timeout': 120,
+    }
+    StandaloneApplication(app, options).run()
