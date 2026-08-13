839392e66a6fa3af2e11148caf6324caea7f65bd
Elvis567890 <mbaziiraelvis0@gmail.com>
2026-08-13 19:05:13 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,1 @@
-import eventlet
-from eventlet import wsgi
-from scraper import app
-
-# Monkey-patch the standard library for non-blocking I/O
-eventlet.monkey_patch()
-
-if __name__ == "__main__":
-    # Run the Flask app using Eventlet's native WSGI server on port 8080
-    # This completely bypasses Gunicorn, avoiding the entry point crash
-    wsgi.server(eventlet.listen(('', 8080)), app)
+from app import app
